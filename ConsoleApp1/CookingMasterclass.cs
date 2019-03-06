97b9fbdd4c89646d47ccc081356fc9a5398c59d3
Nikolay Kelov <n_kelov@yahoo.com>
2019-03-06 11:15:04 +0100

    Problem 1 50 out of 100 points

--- ConsoleApp1/CookingMasterclass.cs
+++ ConsoleApp1/CookingMasterclass.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 
+
 namespace ConsoleApp1
 {
     class CookingMasterclass
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            double budget = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double flourPerPackage = double.Parse(Console.ReadLine());
+            double eggPerPiece = double.Parse(Console.ReadLine());
+            double apronPerPiece = double.Parse(Console.ReadLine());
+
+            double flourBudget = 0;
+            if (students < 5)
+            {
+                flourBudget = students * flourPerPackage;
+            }
+            else
+            {
+                flourBudget = students * (flourPerPackage - (flourPerPackage * 0.2));
+            }
+            
+            double eggBudget = eggPerPiece * 10 * students;
+            double apronBudget = apronPerPiece * (students + (students * 0.2));
+            apronBudget = Math.Ceiling(apronBudget);
+
+            double budgetNeeded = flourBudget + eggBudget + apronBudget;
+
+            if (budget >= budgetNeeded)
+            {
+                Console.WriteLine($"Items purchased for {budgetNeeded:F2}$.");
+            }
+            else
+            {
+                double notEnough = budgetNeeded - budget;
+                Console.WriteLine($"{notEnough:F2}$ more needed.");
+            }
         }
     }
 }
